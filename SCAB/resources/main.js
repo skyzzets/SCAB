b9530cda56cacdf2cb67f7b742563b19abf8fd3c
skyzzets <113076562+skyzzets@users.noreply.github.com>
2023-11-10 18:19:58 -0300

    Teste

--- SCAB/resources/main.js
+++ SCAB/resources/main.js
@@ -1,3 +1,20 @@
-document.getElementById('button-login').addEventListener('click', function() {
+document.getElementById('button-login').addEventListener('click', function () {
     window.location.href = 'index.html';
 });
+
+
+const scrollers = document.querySelectorAll(".scroller");
+
+
+
+
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+}
+
+
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true)
+    });
+}
