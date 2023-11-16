e37d9b099f20792ef42e5a7c80092403be959943
skyzzets <113076562+skyzzets@users.noreply.github.com>
2023-11-16 15:04:59 -0300

    Removed-Current-Car-Folder

--- SCAB/resources/main.js
+++ SCAB/resources/main.js
@@ -1,20 +1,3 @@
 document.getElementById('button-login').addEventListener('click', function () {
     window.location.href = 'index.html';
 });
-
-
-const scrollers = document.querySelectorAll(".scroller");
-
-
-
-
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-}
-
-
-
-function addAnimation() {
-    scrollers.forEach((scroller) => {
-        scroller.setAttribute("data-animated", true)
-    });
-}
