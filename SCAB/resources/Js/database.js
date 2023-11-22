8ee966ac43df8448eca60a9d4e99eac8f5278300
Ian <santiagooiian@gmail.com>
2023-11-22 15:32:47 -0300

    aaaaaaaaaaaaaaaaaaaaaaaa

--- SCAB/resources/Js/database.js
+++ SCAB/resources/Js/database.js
@@ -18,8 +18,9 @@ class CarDetails {
 
 //Load list on Application memory - That contains list of Object 'CarDetails' 
 var CarDetailsList = [];
-CarDetailsList.push(new CarDetails('Huracán', 'Lamborghini', '2021/2022', 'R$ 5.499,00', 'Automatica', 'Branco', 'São Paulo', '1.100 km', '4x2', 'A Lamborghini Huracán personifica a união perfeita entre elegância e desempenho. Seu design escultural e linhas aerodinâmicas sugerem velocidade, enquanto o potente motor V10 proporciona uma experiência de condução visceral. No interior, materiais luxuosos e tecnologia de ponta garantem conforto e sofisticação. Uma obra-prima automotiva que transcende as expectativas, cada detalhe da Huracán é meticulosamente projetado para cativar e inspirar.', 'Trocar depois', 'resources/Storage/AudiA6.jpeg'));
-CarDetailsList.push(new CarDetails('Gallardo', 'Lamborghini', '2010', 'R$ 1.149,99', 'Automatica', 'Branco', 'São Paulo', '18.000 km', '4x2', 'A Lamborghini Gallardo, um ícone de elegância e potência, combina linhas aerodinâmicas sofisticadas com um desempenho excepcional. Seu motor V10 proporciona uma experiência de condução emocionante, enquanto o interior refinado e luxuoso envolve os ocupantes em um ambiente de conforto e estilo. Uma síntese perfeita entre forma e função, a Gallardo é uma obra-prima que transcende a simples definição de carro esportivo.', 'Trocar depois', 'resources/Storage/AudiA6.jpeg'));
+CarDetailsList.push(new CarDetails('Huracán', 'Lamborghini', '2021', 'R$ 5.499,00', 'Automática', 'Branco', 'São Paulo', '1.100 km', '4x2', 'A Lamborghini Huracán personifica a união perfeita entre elegância e desempenho. Seu design escultural e linhas aerodinâmicas sugerem velocidade, enquanto o potente motor V10 proporciona uma experiência de condução visceral. No interior, materiais luxuosos e tecnologia de ponta garantem conforto e sofisticação. Uma obra-prima automotiva que transcende as expectativas, cada detalhe da Huracán é meticulosamente projetado para cativar e inspirar.', '5.2 V10 GASOLINA LP 640 STO LDF', 'resources/Storage/LamborghiniHuracán.jpg'));
+CarDetailsList.push(new CarDetails('Gallardo', 'Lamborghini', '2010', 'R$ 1.149,99', 'Automática', 'Branco', 'São Paulo', '18.000 km', '4x2', 'A Lamborghini Gallardo, um ícone de elegância e potência, combina linhas aerodinâmicas sofisticadas com um desempenho excepcional. Seu motor V10 proporciona uma experiência de condução emocionante, enquanto o interior refinado e luxuoso envolve os ocupantes em um ambiente de conforto e estilo. Uma síntese perfeita entre forma e função, a Gallardo é uma obra-prima que transcende a simples definição de carro esportivo.', '5.2 V10 GASOLINA LP560-4 E-GEAR', 'resources/Storage/LamborghiniGallardo.jpg'));
+CarDetailsList.push(new CarDetails('Urus', 'Lamborghini', '2022', 'R$ 3.390.00', 'Automática', 'Laranja', 'São Paulo', '3.888', '4x4', 'A Lamborghini Urus redefine o conceito de SUV de luxo, incorporando elegância agressiva e desempenho extraordinário. Com linhas aerodinâmicas, seu design expressa sofisticação e poder. O interior combina conforto premium com tecnologia inovadora, proporcionando uma experiência de condução única. Um equilíbrio magistral entre utilidade e exuberância, a Urus personifica a excelência automotiva com uma presença imponente e refinada.', '4.0 V8 TURBO GASOLINA LP650-4 AUTOMÁTICO', 'resources/Storage/LamborghiniUrus.jpg'));
 
 // functions to help optimize querys on database
 
@@ -62,7 +63,7 @@ function showCarDetails(name){
 
     let elementCarDetails = `
         <div class="card animate__animated animate__fadeInUp">
-            <img class="card-img-top card-details-img-height common-img-fluid" src="${elementCar.image}"  alt="${elementCar.name}"/>
+            <img class="card-img-top card-details-img-height common-img-fluid object-fit-cover" src="${elementCar.image}"  alt="${elementCar.name}"/>
             <div class="card-body">
                 <h5>Informações básicas</h5>
                 <div class="row">
