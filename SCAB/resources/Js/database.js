0f0c055d543847f087072fdd2f14741e55230a22
Joao Mantovani <joao.octavio.mantovani@gmail.com>
2023-11-22 01:03:44 -0300

    New Version

---
+++ SCAB/resources/Js/database.js
@@ -0,0 +1,114 @@
+// Init dataset mock
+class CarDetails {
+    constructor(name, model, year, price, transmission, color, location, km, traction, description, summary, image) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.transmission = transmission;
+        this.color = color;
+        this.location = location;
+        this.km = km;
+        this.traction = traction;
+        this.description = description;
+        this.summary = summary;
+        this.image = image;
+    }
+}
+
+//Load list on Application memory - That contains list of Object 'CarDetails' 
+var CarDetailsList = [];
+CarDetailsList.push(new CarDetails('Huracán', 'Lamborghini', '2021/2022', 'R$ 5.499,00', 'Automatica', 'Branco', 'São Paulo', '1.100 km', '4x2', 'A Lamborghini Huracán personifica a união perfeita entre elegância e desempenho. Seu design escultural e linhas aerodinâmicas sugerem velocidade, enquanto o potente motor V10 proporciona uma experiência de condução visceral. No interior, materiais luxuosos e tecnologia de ponta garantem conforto e sofisticação. Uma obra-prima automotiva que transcende as expectativas, cada detalhe da Huracán é meticulosamente projetado para cativar e inspirar.', 'Trocar depois', 'resources/Storage/AudiA6.jpeg'));
+CarDetailsList.push(new CarDetails('Gallardo', 'Lamborghini', '2010', 'R$ 1.149,99', 'Automatica', 'Branco', 'São Paulo', '18.000 km', '4x2', 'A Lamborghini Gallardo, um ícone de elegância e potência, combina linhas aerodinâmicas sofisticadas com um desempenho excepcional. Seu motor V10 proporciona uma experiência de condução emocionante, enquanto o interior refinado e luxuoso envolve os ocupantes em um ambiente de conforto e estilo. Uma síntese perfeita entre forma e função, a Gallardo é uma obra-prima que transcende a simples definição de carro esportivo.', 'Trocar depois', 'resources/Storage/AudiA6.jpeg'));
+
+// functions to help optimize querys on database
+
+function findCarDetailsByName(name){
+    let lowerCaseName = name.toLowerCase();
+    return CarDetailsList.find(car => car.name.toLowerCase().includes(lowerCaseName));
+}
+
+function showCarDetailsPrice(name){
+    let sectionId = "car-details-price";
+
+    let elementCar = findCarDetailsByName(name);
+
+    let elementCarDetailsPrice = `
+        <div class="card card-price-height animate__animated animate__fadeInRight">
+            <div class="card-body">
+                <div>
+                    <h5 class="card-title"> ${elementCar.model} <span class="common-text-marked">${elementCar.name}</span> - ${elementCar.summary} - ${elementCar.year}</h5>
+                    <span class="common-divisor mb-3" style="width: 100%;"></span>
+                    <p class="card-subtitle mb-2 text-body-secondary">${elementCar.km} • ${elementCar.location}</p>
+                </div>
+                <h5 class="mt-2">${elementCar.price} - ao dia</h5>
+                <div class="mt-5 mb-3">
+                    <a href="#" class="btn btn-main w-100 mb-2"><i class="bi bi-check-circle-fill"></i> Fazer Reserva</a>
+                    <a href="#" class="btn btn-main w-100"><i class="bi bi-heart-fill"></i> Add. Favoritos</a>
+                </div>
+                <a href="index.html" class="text-muted d-flex justify-content-end"> Voltar para a página inicial</a>
+            </div>
+        </div>
+    `;
+
+    // Inserindo o elemento no section Id
+    document.getElementById(sectionId).innerHTML = elementCarDetailsPrice;
+}
+
+function showCarDetails(name){
+    let sectionId = "car-details";
+
+    let elementCar = findCarDetailsByName(name);
+
+    let elementCarDetails = `
+        <div class="card animate__animated animate__fadeInUp">
+            <img class="card-img-top card-details-img-height common-img-fluid" src="${elementCar.image}"  alt="${elementCar.name}"/>
+            <div class="card-body">
+                <h5>Informações básicas</h5>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <p class="text-muted mb-0">Acionamento das rodas</p>
+                        <p class="card-text">${elementCar.traction}</p>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <p class="text-muted mb-0">Transmissão</p>
+                        <p class="card-text">${elementCar.transmission}</p>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <p class="text-muted mb-0">Kms</p>
+                        <p class="card-text">${elementCar.km}</p>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <p class="text-muted mb-0">Loja</p>
+                        <p class="card-text">AutoBahn - ${elementCar.location}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    document.getElementById(sectionId).innerHTML = elementCarDetails;
+}
+
+function showErrorSearc(){
+    let sectionId = `car-search-error`;
+    
+    let elementError = `
+        <div class="col-md-12">
+            <div class="card animate__animated animate__fadeInUp">
+                <div class="card-body text-center">
+                    <h5>Ops! Carro não encontrado!</h5>
+                    <p class="text-muted mb-0">Não conseguimos encontrar o carro que deseja, tente pesquisar por outro modelo. - Ex: 'Gallardo', 'Huracán' e etc...</p>
+                    
+                </div>
+            </div>
+        </div>
+    `;
+
+    document.getElementById(sectionId).innerHTML = elementError;
+}
+
+function showCar(name){
+    showCarDetails(name);
+    showCarDetailsPrice(name);
+}
--- SCAB/resources/Js/main.js
+++ SCAB/resources/Js/main.js
@@ -1,38 +1,51 @@
-// Initi boostrap plugin -  tool tip
+
+// Init - boostrap tool tip plugin
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-// Initi plugin - animate.css
-const elementParagraph = document.querySelector('.container .header-title');
-
-elementParagraph.addEventListener('click', () => {
-    elementParagraph.classList.add('animate__animated', 'animate__rubberBand');
-
-    setTimeout(() => {
-        elementParagraph.classList.remove('animate__animated', 'animate__rubberBand');
-
-    }, 2000);
-});
-
-// Initi menu - side bar
+// Init - navigation menu plugin
 function GetCurrentUrl() {
     return window.location.pathname.split("/").pop();
 }
 
-function GoToPageSearchCar() {
+function GetParameterByQueryString() {
+    let url = window.location.href;
+    
+    let params = new URLSearchParams(url.split('?')[1]);
+    
+    return params.get(`name`) || null;
+}
+
+function GoToHomePage(){
     let pageRedirect = "index.html";
 
     window.location.href = pageRedirect;
 }
 
-function GoToPageListCar(){
+function GoToSearchCarPage(name) {
+    let pageRedirect = "details.html";
+    
+    if(name){ 
+        pageRedirect = `details.html?name=${name}`
+    }
+    
+    window.location.href = pageRedirect;
+}
+
+function GoToListCarPage() {
     let pageRedirect = "index.html#section-list-car"
 
     window.location.href = pageRedirect;
 }
 
-function GoToPageReservationCar(){
-    let pageRedirect ="details.html"
+function GoToReservationCarPage() {
+    let pageRedirect = "details.html"
+
+    window.location.href = pageRedirect;
+}
+
+function GoToLoginPage(){
+    let pageRedirect = "login.html";
 
     window.location.href = pageRedirect;
 }
