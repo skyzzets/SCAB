66b9d4bf04c7c93a89d1c09e8882133ed90d2703
skyzzets <113076562+skyzzets@users.noreply.github.com>
2023-12-01 19:48:12 -0300

    new commit

--- SCAB/resources/Js/main.js
+++ SCAB/resources/Js/main.js
@@ -10,13 +10,13 @@ function GetCurrentUrl() {
 
 function GetParameterByQueryString() {
     let url = window.location.href;
-    
+
     let params = new URLSearchParams(url.split('?')[1]);
-    
+
     return params.get(`name`) || null;
 }
 
-function GoToHomePage(){
+function GoToHomePage() {
     let pageRedirect = "index.html";
 
     window.location.href = pageRedirect;
@@ -24,11 +24,11 @@ function GoToHomePage(){
 
 function GoToSearchCarPage(name) {
     let pageRedirect = "details.html";
-    
-    if(name){ 
+
+    if (name) {
         pageRedirect = `details.html?name=${name}`
     }
-    
+
     window.location.href = pageRedirect;
 }
 
@@ -44,8 +44,67 @@ function GoToReservationCarPage() {
     window.location.href = pageRedirect;
 }
 
-function GoToLoginPage(){
+function GoToLoginPage() {
     let pageRedirect = "login.html";
 
     window.location.href = pageRedirect;
 }
+
+const track = document.getElementById("image-track")
+
+window.onmousedown = e => {
+    track.dataset.mouseDownAt = e.clientX;
+}
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+
+
+window.onmousemove = e => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const containerRect = track.parentElement.getBoundingClientRect();
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX + containerRect.left;
+    const maxDelta = containerRect.width / 2;
+
+    const percentage = (mouseDelta / maxDelta) * -100;
+    const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    const animationDuration = 2500;
+
+    track.style.transition = `transform ${animationDuration}ms ease-out`;
+    track.style.transform = `translateX(${nextPercentage}%)`;
+
+    for (const image of track.getElementsByClassName("image")) {
+        image.style.transition = `object-position ${animationDuration}ms ease-out`;
+        image.style.objectPosition = `${100 + nextPercentage}% center`;
+    }
+}
+
+
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+
